19128947fb4634f2efd7c4761c304c5e738d7b66
arulson <jebbyarulson@gmail.com>
2025-11-12 23:50:33 -0800

    switch the names of vendor and vendor_og

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/vendor_og/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/vendor_og/keymap.c
@@ -33,11 +33,12 @@ enum dilemma_keymap_layers {
 
 #define ESC_MED LT(LAYER_MEDIA, KC_ESC)
 #define SPC_NAV LT(LAYER_NAVIGATION, KC_SPC)
-#define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
+#define F_FUN LT(LAYER_FUNCTION, KC_F)
 #define ENT_SYM LT(LAYER_SYMBOLS, KC_ENT)
 #define BSP_NUM LT(LAYER_NUMERAL, KC_BSPC)
-#define PT_Z LT(LAYER_POINTER, KC_Z)
-#define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
+#define PT_P LT(LAYER_POINTER, KC_P)
+#define PT_B LT(LAYER_POINTER, KC_B)
+
 
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL KC_NO
@@ -46,14 +47,23 @@ enum dilemma_keymap_layers {
 #    define SNIPING KC_NO
 #endif // !POINTING_DEVICE_ENABLE
 
+const key_override_t comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_QUES);
+const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_MINUS);
+
+// This globally defines all key overrides to be used
+const key_override_t *key_overrides[] = {
+  &comm_key_override,
+  &dot_key_override
+};
+
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x5_3(
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-       LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT),
-       PT_Z,    RALT_T(KC_X),    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), PT_SLSH,
-                      ESC_MED, TAB_FUN, SPC_NAV, ENT_SYM, BSP_NUM, KC_MUTE
+       KC_Q,          KC_X,         KC_G,         KC_M,         KC_K,     KC_SCLN,  KC_COMM,      KC_QUOT,      KC_J,         KC_Z,
+       LGUI_T(KC_N),  LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_W,     KC_DOT,   LCTL_T(KC_A), LSFT_T(KC_E), LALT_T(KC_I), LGUI_T(KC_H),
+       PT_P,          KC_L,         KC_C,         KC_D,         KC_V,     KC_TAB,   KC_U,         KC_O,         KC_Y,         PT_B,
+                                    F_FUN,        BSP_NUM,      ESC_MED,  QK_CAPS_WORD_TOGGLE,  SPC_NAV,      ENT_SYM
   ),
 
 /*
@@ -75,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * from the base layer to enable auto-repeat.
  */
   [LAYER_FUNCTION] = LAYOUT_split_3x5_3(
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_SCRL,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
-    XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10,
-                      XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    _______, _______, _______, _______, _______,   KC_MUTE,   KC_F7, KC_F8, KC_F9,  KC_F12,
+    _______, _______, _______, _______, _______,   KC_VOLU,   KC_F4, KC_F5, KC_F6,  KC_F11,
+    _______, _______, _______, _______, _______,   KC_VOLD,   KC_F1, KC_F2, KC_F3,  KC_F10,
+                      _______, _______, _______,   KC_MPRV,   KC_MNXT, _______
   ),
 
 /**
@@ -90,11 +100,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * base layer to avoid having to layer change mid edit and to enable auto-repeat.
  */
   [LAYER_NAVIGATION] = LAYOUT_split_3x5_3(
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,
-    XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,
-                      XXXXXXX, XXXXXXX, _______,  KC_ENT, KC_BSPC, KC_DEL
+    _______, _______, _______, _______, KC_INS,     _______, KC_BTN1, KC_BTN2, KC_BTN3, _______,
+    KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_BRIU,    _______, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI,
+    KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_BRID,    _______, _______, _______, _______, _______,
+                      _______, KC_ESC,  KC_DEL,     _______, _______, _______
   ),
+
 
 /**
  * \brief Media layer.
@@ -125,11 +136,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `KC_DOT` is duplicated from the base layer.
  */
   [LAYER_NUMERAL] = LAYOUT_split_3x5_3(
-    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-     KC_DOT,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
-                       KC_DOT, KC_MINS,    KC_0, XXXXXXX, _______, XXXXXXX
+    _______,   _______,   _______,   _______,   _______,   KC_PLUS, KC_7,   KC_8,   KC_9,   KC_ASTR,
+    KC_LGUI,   KC_LALT,   KC_LSFT,   KC_LCTL,   _______,   KC_DOT,  KC_4,   KC_5,   KC_6,   KC_0,
+    _______,   _______,   _______,   _______,   _______,   KC_EQL,  KC_1,   KC_2,   KC_3,   KC_SLSH,
+                          _______,   _______,   _______,   KC_MPLY, KC_SPC, _______
   ),
+
 
 /**
  * \brief Symbols layer.
@@ -138,12 +150,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * chording when using mods with shifted symbols. `KC_LPRN` is duplicated next to
  * `KC_RPRN`.
  */
+
   [LAYER_SYMBOLS] = LAYOUT_split_3x5_3(
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
-                      KC_RPRN,  KC_GRV, KC_UNDS, _______, XXXXXXX, XXXXXXX
+    _______,   _______,   _______,  _______,  _______,   _______, _______, _______, _______, _______, 
+    KC_LBRC,   KC_RBRC,   KC_LCBR,  KC_RCBR,  KC_TILD,   _______, _______, _______, _______, _______, 
+    KC_LABK,   KC_RABK,   KC_LPRN,  KC_RPRN,  KC_GRV,    _______, _______, _______, _______, _______, 
+                          _______,  KC_UNDS,  KC_BSLS,   _______, _______, _______
   ),
+
 };
 // clang-format on
 
