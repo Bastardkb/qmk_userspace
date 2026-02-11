4183716f3bef81f0d1b6d661cd411b1b18aa70ab
jwc920 <jwc920github@gmail.com>
2026-02-11 15:08:34 -0800

    some rearrangements

--- keyboards/bastardkb/scylla/keymaps/jason-scylla/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/jason-scylla/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_4x6_5(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TG(1),
-        KC_ESC, KC_K,    KC_H,    KC_C,    KC_D,    KC_W,             KC_SLSH, KC_U,    KC_O,    KC_Y,    KC_Q,    KC_BSLS,
+        KC_ESC, KC_K,    KC_H,    KC_C,    KC_D,    KC_W,             S(KC_SLSH), KC_U,    KC_O,    KC_Y,    KC_Q,    KC_BSLS,
         KC_TAB, LCTL_T(KC_L),    LALT_T(KC_N),    LGUI_T(KC_S),    LSFT_T(KC_T),    KC_G,             KC_J, LSFT_T(KC_A),    LGUI_T(KC_E),    LALT_T(KC_I),    LCTL_T(KC_P),    S(KC_QUOT),
         KC_CAPS, KC_V,    KC_B,    KC_F,    KC_M,    KC_X,             KC_MINS, KC_QUOT,  KC_COMM, KC_DOT, KC_Z,  KC_TRNS,
                              MO(2), LT(3, KC_R), KC_TRNS, KC_ENT, KC_SPC, KC_BSPC, KC_SPC, KC_BSPC, KC_TRNS, KC_TRNS
@@ -63,17 +63,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_4x6_5(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, C(S(KC_TAB)), C(KC_TAB), OS_APPP, OS_APPN, KC_TRNS,   KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS,          KC_TRNS, OS_SWDL, SEL_LINE, OS_SWDR, KC_TRNS, KC_TRNS,
-        KC_TRNS, OS_UNDO, OS_CUT,  OS_COPY, OS_PASTE, OS_REDO,          KC_TRNS, OS_WRDL, KC_TRNS, OS_WRDR, KC_TRNS, QK_BOOT,
+        KC_TRNS, C(S(KC_TAB)), C(KC_TAB), OS_APPP, OS_APPN, KC_TRNS,   KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS,
+        KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS,          SEL_LINE, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, EE_CLR,
+        KC_TRNS, OS_UNDO, OS_CUT,  OS_COPY, OS_PASTE, OS_REDO,         OS_WRDL, A(KC_BSPC), A(KC_DEL), OS_WRDR, KC_DEL, QK_BOOT,
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [3] = LAYOUT_split_4x6_5(
         KC_TRNS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),          S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_TRNS,
-        KC_TRNS, S(KC_7), S(KC_LBRC), S(KC_MINS), S(KC_RBRC), KC_GRV, S(KC_7), S(KC_SLSH), KC_UP, S(KC_BSLS), KC_BSLS, KC_TRNS,
-        KC_TRNS, S(KC_6), S(KC_9), KC_EQL, S(KC_0), S(KC_SCLN),        S(KC_GRV), KC_LEFT, KC_DOWN, KC_RIGHT, KC_SCLN, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_LBRC, S(KC_EQL), KC_RBRC, KC_TRNS,       KC_TRNS, S(KC_COMM), KC_MINS, S(KC_DOT), KC_SLSH, KC_TRNS,
+        KC_TRNS, S(KC_6), S(KC_LBRC), S(KC_MINS), S(KC_RBRC), KC_GRV, S(KC_7), S(KC_SLSH), KC_UP, S(KC_BSLS), KC_BSLS, KC_TRNS,
+        KC_TRNS, KC_LCTL, LALT_T(KC_LBRC), LGUI_T(KC_EQL), LSFT_T(KC_RBRC), S(KC_SCLN), S(KC_GRV), KC_LEFT, KC_DOWN, KC_RIGHT, KC_SCLN, KC_TRNS,
+        KC_TRNS, S(KC_6), S(KC_COMM), S(KC_MINS), S(KC_DOT), S(KC_DOT),       KC_TRNS, S(KC_9), S(KC_EQUAL), S(KC_0), KC_SLSH, KC_TRNS,
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
